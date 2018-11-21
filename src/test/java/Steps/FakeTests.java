ba7ece00f415e41803d3e890aeeda9e2e3f1dad1
Justyna Kurnik <ext-justyna.kurnik@finago.com>
2018-11-21 10:19:52 +0100

    commit chnges

--- src/test/java/Steps/FakeTests.java
+++ src/test/java/Steps/FakeTests.java
@@ -1,15 +1,21 @@
 package Steps;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.springframework.context.annotation.Description;
 import org.testng.annotations.Test;
 
+@Feature("szkolenie Appium")
 public class FakeTests {
-    @Test(groups = {"smoke","regression"})
-    public void fakeSmokeTest(){
+    @Test(groups = {"smoke", "regression"})
+    @Story("this is fake test")
+    @Description("this test will never fail")
+    public void fakeSmokeTest() {
 
     }
 
     @Test(groups = {"regression"})
-    public void fakeRegressionTest(){
+    public void fakeRegressionTest() {
 
     }
 }
